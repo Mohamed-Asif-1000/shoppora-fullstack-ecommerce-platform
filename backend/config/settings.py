d5e815a4dc98b36732a79d7f83f68ebefecef955
Mohamed Asif.A <atmcreationsofficial@gmail.com>
2026-09-04 19:25:49 +0530

    fix cloudinary collecstatic compatibility

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -126,6 +126,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 CLOUDINARY_STORAGE = {'CLOUDINARY_URL': config('CLOUDINARY_URL')}
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STORAGES = {"default": {"BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage"},
             "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"}, }
 
