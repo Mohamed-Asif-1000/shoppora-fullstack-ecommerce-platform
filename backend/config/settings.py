904c344025a83fd07c6c8560e6cf6dec1becc9c5
Mohamed Asif.A <atmcreationsofficial@gmail.com>
2026-09-04 17:51:52 +0530

    add Vercel frontend url to csrf trusted origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -133,7 +133,9 @@ CORS_ALLOWED_ORIGINS = config(
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5173', 'http://127.0.0.1:5173',
-                        'https://considerate-nature-production-d83c.up.railway.app', ]
+                        'https://considerate-nature-production-d83c.up.railway.app',
+                        'https://shoppora-marketplacevercelapp.vercel.app', ]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
